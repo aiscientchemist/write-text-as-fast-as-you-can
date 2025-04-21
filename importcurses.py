f615a3f7054cd17bb5660bc361e0d7c9a89d171f
aiscientchemist <aiscientchemist@gmail.com>
2025-04-21 17:08:00 +0200

    x

---
+++ importcurses.py
@@ -0,0 +1,12 @@
+import curses
+from curses import wrapper
+
+
+def main(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Hello World!")
+    stdscr.refresh()
+    stdscr.getkey()
+
+
+wrapper(main)
